b87d52fdc1d96d6bae4c8179d30712ce689d502c
jeffersonsysage <jeffersondeng@metaage.com.tw>
2024-12-12 03:25:54 +0000

    update

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/test")
+@app.route("/api")
 def test():
     return jsonify("This message is from Backend!")
 if __name__ == "__main__":
